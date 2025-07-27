6b311fbd19a6297901e289626f6d6f2073e2b5ad
Ravindra Julakanti <yadav.ravindra64@gmail.com>
2025-07-27 10:09:15 +0530

    Add files via upload

---
+++ Module-3/elilfor.py
@@ -0,0 +1,5 @@
+user_country = input('what is your country: ')
+if user_country == 'Germany' or user_country == 'Denmark' or user_country == 'Sweden':
+    print('You are eligible for the tri student exchange programme')
+else:
+    print('Sorry, you are not eligible')
